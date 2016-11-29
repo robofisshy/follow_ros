01f6b458404818150f91cc28950967b3e8607730
yyq <zju_yyq@163.com>
2016-11-29 14:43:06 +0800

    update

--- follow.sh
+++ follow.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-export LD_LIBRARY_PATH=/opt/OpenNI-Linux-ARM-2.2/Redist 
+export LD_LIBRARY_PATH=/opt/OpenNI-Linux-ARM-2.2/Redist
 source /opt/ros/indigo/setup.bash;
-roscore&
-sleep 10s
 roslaunch turtlebot_bringup minimal.launch&
-sleep 10s
-source /opt/project/catkin_ws/devel/setup.bash&& \
-rosrun follow follow 
-while(1)
+sleep 5s;
+source /opt/project/catkin_ws/devel/setup.bash&&
+rosrun follow follow
+
